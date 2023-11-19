fa71f6e8679a2b8b53081c8ad7082c1044d7a437
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-19 16:51:04 +0530

    Add files via upload

--- pattern practice 19.11/Program.cs
+++ pattern practice 19.11/Program.cs
@@ -10,35 +10,30 @@ namespace pattern_practice_19._11
     {
         static void Main(string[] args)
         {
-            /*
-                        for (int i  = 1; i <=4;i++)
-                        {
-                            for (int j  = 1; j <= i; j++)
-                            {
-                                Console.Write("*");
-                            }
-                            Console.WriteLine();    
-                        }*/
 
-            for(int i = 1; i <=5; i++)
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 1; i < n; i++)
             {
-                for (int j = 1; j <= 5-i; j++)
-                {
-                    Console.Write(" ");
-                }
-                for(int k = 1; k <= i; k++)
-                {
-                    Console.Write("*");
-                }
-
+                for (int j = 1; j <= i; j++)
+                    Console.Write(j.ToString());
                 Console.WriteLine();
-
             }
+            for (int i = n; i >= 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write(j.ToString());
+                Console.WriteLine();
+            }
+            Console.WriteLine();
 
 
 
-
-
-        }    
+            Console.ReadLine();
+        }   
+       
     }
 }
