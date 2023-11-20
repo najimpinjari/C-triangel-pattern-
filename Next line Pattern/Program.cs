d51fb5290cefc75e0e5c003ae80de1fc4c37cc33
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-20 22:06:24 +0530

    Add files via upload

---
+++ Next line Pattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malti_dimensional_Array.@new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = new int[4, 4];
+            
+            for(int i = 0; i < 4; i ++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j)
+                        Console.Write($"*\t");
+
+                    else
+                    {
+                        Console.Write("\t");
+                    } 
+
+                }
+                Console.WriteLine();// for new lin
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
