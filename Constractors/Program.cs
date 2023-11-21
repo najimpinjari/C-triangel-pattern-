484d64b3a5791070bfc51314a447522f0a3fa4ae
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-21 18:44:59 +0530

    Add files via upload

---
+++ Constractors/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constractors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student1 s1 = new Student1();
+            s1.Details();
+
+            //fist way to initialization object 
+
+            Student1 s2 = new Student1();
+            s2.Rollnumber = 20;
+            s2.FirstName = "Abhisek";
+            s2.LastName = "sutar";
+            s2.Details();
+
+            //second way to initialization object 
+
+            Student1 s3 = new Student1()
+            {
+                Rollnumber = 22,
+                FirstName = "Abhisek",
+                LastName = "sutar"
+            }; 
+            s3.Details();
+
+            Student1 s4 = new Student1();
+            s4.initialize(12,"arbaz" , "mujjavar");
+            s4.Details();
+
+            Student1 s5 = new Student1(11,"vivek" , "pawar");
+            s5.Details();
+
+            Student1 s6 = s5;
+            s6.Details();
+
+            Student1 s7 = new Student1(s4);
+              s7.Details();
+            /*
+                        Student1 s8 = new Student1();
+                        s8.Details();
+                        Student1 s9 = new Student1();  
+                        s9.Details();
+                        Student1 s10 = new Student1();
+                        s10.Details();
+            */
+            //Student1 s11 = new Student1();
+            //Student1 s12 = new Student1();
+
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Constractors/Student1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constractors
+{
+    public class Student1
+    {
+        public int Rollnumber;
+        public string FirstName;
+        public string LastName;
+        public string coursename = "dotnet";
+
+        public void Details()
+        {
+            Console.WriteLine($"Rollnumber ; {Rollnumber} fullName ;{FirstName} {LastName} {coursename}");
+          
+        }
+
+        public Student1()
+        {
+            Console.WriteLine("Public  Student() Constructor Called ");
+            Rollnumber = 3;
+            FirstName = "rupesh";
+            LastName = "pawar";
+           coursename = "dot.net";
+        }
+
+
+
+        public void initialize ( int rn, string fn , string ln)
+        {
+            Rollnumber = rn;
+            FirstName = fn;
+            LastName = ln;
+        }
+        //paameterize constructor 
+        public Student1(int rn , string fn , string ln)
+        {
+            Console.WriteLine("Student1(int rn , string fn , string ln) Constructor called ");
+            Rollnumber = rn;
+            FirstName = fn;
+            LastName = ln;
+        }
+
+        //copy constructor 
+        public Student1(Student1 s)
+        {
+            this.Rollnumber = s.Rollnumber;
+            this.FirstName = s.FirstName;
+            this.LastName = s.LastName;
+        }
+
+        //static constructor    
+        static Student1()
+        {
+            Console.WriteLine("Public  Student() Constructor Called ");
+        }
+    }
+}
