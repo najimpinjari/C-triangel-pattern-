c66a973ae27748fec79f585128e6cc20165259ed
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-21 18:47:40 +0530

    Add files via upload

---
+++ holo retangle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace holo_retangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+
+
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 1; j <= 2 * (n - i); j++)
+                {
+                    Console.Write(" ");
+                }
+                          
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 4; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 1; j <= 2 * (n - i); j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
